4d91ef4bd24f78db71ef6f7a25775ac5658c3db7
paulerlic <86337035+paulerlic@users.noreply.github.com>
2022-03-10 23:08:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# Import Flask, PyMongo, and scraping.py 
+# Import 
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
@@ -10,7 +10,7 @@ app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 
-#app routing
+#app route
 @app.route("/")
 def index():
    mars = mongo.db.mars.find_one()
@@ -24,8 +24,9 @@ def scrape():
    mars.update({}, mars_data, upsert=True)
    return redirect('/', code=302)
 
- #.update(query_parameter, data, options) 
-#mars_data.update({}, mars_data, upsert=True)   
+ #mars_data.update({}, mars_data, upsert=True)   
+ #(query_parameter, data, options) 
+
 
 
 if __name__ == "__main__":
